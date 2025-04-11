36c15a2209814f1fb2bc1c2fdc1f27675cbd9fff
Abdullah Ahmed <abdallahsherdy@gmail.com>
2025-04-11 10:03:42 +0200

    Initial Spring Boot project setup

---
+++ project/src/main/java/com/JobifyHub/employment_system/JobifyHubApplication.java
@@ -0,0 +1,13 @@
+package com.JobifyHub.employment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobifyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobifyHubApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/com/JobifyHub/employment_system/JobifyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobifyHub.employment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobifyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
